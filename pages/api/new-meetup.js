974a28885246c36ac62003646eaa84cbdea4f96b
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-09-09 19:40:22 +0530

    API routes and connecting MongoDB database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -10,14 +10,18 @@ async function handler(req, res) {
     //const { title, image, address, description } = data;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://massimo:fxYlwrFHOjprqkqb@cluster0.4lwho.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://massimo:fxYlwrFHOjprqkqb@cluster0.4lwho.mongodb.net/meetups?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     );
 
-    const db = client.db;
+    const db = client.db();
 
     const meetupsCollection = db.collection("meetupsColl");
 
-    const result = meetupsCollection.insertOne(data);
+    const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
 
